37864fbc0f8714630a6ff9b9fb6debc006a4de46
Emily Nelson <evnelson2021@gmail.com>
2022-10-21 16:45:01 -0500

    Finished Brewed Awakenings

---
+++ scripts/Employees.js
@@ -0,0 +1,43 @@
+import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
+
+const employees = getEmployees()
+const orders = getOrders()
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
+const countingOrders = (employee) =>{
+    let numberOrders = 0
+    for (const order of orders){
+            if (order.employeeId === employee.id){
+                numberOrders++
+            }
+        }    
+    return numberOrders
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const numberSold = countingOrders(employee)
+                    window.alert(`${employee.name} sold ${numberSold}`)
+                }
+            }
+        }
+    }
+)
